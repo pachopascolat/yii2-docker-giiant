3add6b572c447a1430aa4ad01d83ed9a8b5da9b7
pachopascolat <patriciopascolat@gmail.com>
2021-03-09 17:56:31 -0300

    Agregado el cors

---
+++ app/controllers/api/ActiveController.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace app\controllers;
+
+use yii\rest\ActiveController as Controller;
+
+
+
+class ActiveController extends Controller
+{
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+
+        // remove authentication filter
+        $auth = $behaviors['authenticator'];
+        unset($behaviors['authenticator']);
+
+        // add CORS filter
+        $behaviors['corsFilter'] = [
+            'class' => \yii\filters\Cors::className(),
+        ];
+
+        // re-add authentication filter
+        $behaviors['authenticator'] = $auth;
+        // avoid authentication on CORS-pre-flight requests (HTTP OPTIONS method)
+        $behaviors['authenticator']['except'] = ['options'];
+
+        return $behaviors;
+    }
+
+}
